cc5dd1969231e0957e8f548e6cb3004c23e0fcc2
Bulveyz <34393239+Bulveyz@users.noreply.github.com>
2018-03-13 09:20:05 +0600

    Update commander.js

--- vendor/bulveyz/bulveyz/src/Bulveyz/Commander/commander.js
+++ vendor/bulveyz/bulveyz/src/Bulveyz/Commander/commander.js
@@ -14,10 +14,11 @@ jQuery(document).ready(function($) {
     }
     else if (command == 'make controller') {
       term.push(function (command, term) {
-            $.post('http://bloge.test/bcommander/makecontroller', {command: command}).then(function(response) {
+            $.post('/bcommander/makecontroller', {command: command}).then(function(response) {
               if (response === '')
               {
                 term.echo(command.charAt(0).toUpperCase() + command.slice(1) + 'Controller created');
+                term.pop();
               }
               else {
                 term.error(response);
@@ -30,11 +31,15 @@ jQuery(document).ready(function($) {
           });
     }
     else if (command == 'make model') {
+      var history = term.history();
+      history.disable();
       term.push(function (command, term) {
-            $.post('http://bloge.test/bcommander/makemodel', {command: command}).then(function(response) {
+            $.post('/bcommander/makemodel', {command: command}).then(function(response) {
               if (response === '')
               {
                 term.echo(command.charAt(0).toUpperCase() + command.slice(1) + ' model created');
+                term.pop();
+                history.enable();
               }
               else {
                 term.error(response);
@@ -48,10 +53,11 @@ jQuery(document).ready(function($) {
     }
     else if (command == 'make -c -m') {
       term.push(function (command, term) {
-            $.post('http://bloge.test/bcommander/makecontrollerandmodel', {command: command}).then(function(response) {
+            $.post('/bcommander/makecontrollerandmodel', {command: command}).then(function(response) {
               if (response === '')
               {
                 term.echo(command.charAt(0).toUpperCase() + command.slice(1) + 'Controller and model created');
+                term.pop();
               }
               else {
                 term.error(response);
@@ -64,7 +70,7 @@ jQuery(document).ready(function($) {
           });
     }
     else if (command == 'make auth') {
-      $.get('http://bloge.test/bcommander/makeauth', {command: command}).then(function(response) {
+      $.get('/bcommander/makeauth', {command: command}).then(function(response) {
         if (response === '')
         {
           term.echo('Auth created');
@@ -76,10 +82,11 @@ jQuery(document).ready(function($) {
     }
     else if (command == 'table trash rows') {
       term.push(function (command, term) {
-        $.post('http://bloge.test/bcommander/trashall', {command: command}).then(function(response) {
+        $.post('/bcommander/trashall', {command: command}).then(function(response) {
           if (response === '')
           {
             term.echo('Table rows clear');
+            term.pop();
           }
           else {
             term.error(response);
@@ -89,10 +96,11 @@ jQuery(document).ready(function($) {
     }
     else if (command == 'new admin') {
       term.push(function (command, term) {
-            $.post('http://bloge.test/bcommander/newadmin', {command: command}).then(function(response) {
+            $.post('/bcommander/newadmin', {command: command}).then(function(response) {
               if (response === '')
               {
                 term.echo('admin added');
+                term.pop();
               }
               else {
                 term.error(response);
@@ -110,5 +118,3 @@ jQuery(document).ready(function($) {
     greetings: "Bulveyz Commander ('help' for all commands)"
   });
 });
-
-console.log('asdasd'.charAt(0).toUpperCase() + 'asdasd'.slice(1))
